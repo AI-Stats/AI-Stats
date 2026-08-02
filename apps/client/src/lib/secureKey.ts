f4ff1b9aceeefad1b3da1d90040091df6bf5bcf9
Daniel <72151886+DanielButler1@users.noreply.github.com>
2026-08-02 19:18:36 -0400

    chore: remove superseded workspace path (18/21)

--- apps/client/src/lib/secureKey.ts
+++
@@ -1,8 +0,0 @@
-import * as SecureStore from "expo-secure-store";
-
-const KEY = "phaseo.user-api-key";
-export const secureKey = {
-  get: () => SecureStore.getItemAsync(KEY, { requireAuthentication: false }),
-  set: (value: string) => SecureStore.setItemAsync(KEY, value, { keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY }),
-  clear: () => SecureStore.deleteItemAsync(KEY)
-};
