57b1a1f2a22e4223fba879192b49933c725f8a56
Daniel Butler <72151886+DanielButler1@users.noreply.github.com>
2026-09-05 11:43:22 +0100

    style(db): align function patch indentation

--- supabase/migrations/20260905095900_repair_pending_provider_retirement_migration.sql
+++ supabase/migrations/20260905095900_repair_pending_provider_retirement_migration.sql
@@ -46,8 +46,8 @@ begin
     );
     patched := replace(
       patched,
-        '        ''data_region'', model.data_region',
-        '        ''data_region'', model.data_region,
+      '        ''data_region'', model.data_region',
+      '        ''data_region'', model.data_region,
         ''effective_from'', model.effective_from,
         ''effective_to'', model.effective_to'
     );
